31343438f7bae6627b1f98bd14f6c37c29af1a65
farukumarx64 <f.umaridris.mail@gmail.com>
2023-11-01 13:48:18 +0100

    initial confirmation page commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { v4 as uuidv4 } from 'uuid';
+import ConfirmationPage from './components/ConfirmationPage';
 
 function App() {
   const a = uuidv4(), b = uuidv4();
@@ -7,6 +8,7 @@ function App() {
 
   return (
     <>
+    <ConfirmationPage />
     </>
   )
 }
--- src/components/ConfirmationPage.jsx
+++ src/components/ConfirmationPage.jsx
@@ -1,3 +1,22 @@
+import CircularProgress from "./animations/CircularProgress";
+import { motion, useMotionValue} from "framer-motion"
+
 export default function ConfirmationPage() {
-  
+  const progress = useMotionValue(90)
+  return (
+    <div className="confirmation-container">
+      <motion.div
+        initial={{ x: 0 }}
+        animate={{ x: 100 }}
+        style={{ x: progress }}
+        transition={{ duration: 1 }}
+      />
+      <CircularProgress progress={progress} />
+      <h2>Complaint Submitted Successfully!</h2>
+      <p>
+        Thank you for submitting your complaint. Our team will review it
+        shortly.
+      </p>
+    </div>
+  );
 }
